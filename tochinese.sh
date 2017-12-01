f0d112ccc44ce0b3db16cd470c0ce35db4eaf626
cyouk2 <q174221679@gmail.com>
2017-12-01 10:53:00 +0900

    Add files via upload

---
+++ tochinese.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ADDITIONAL_PATH="/root/wow_db_chinese/"
+DB_HOST="localhost"
+DB_PORT="3306"
+DATABASE="mangos"
+USERNAME="mangos"
+PASSWORD="mangos"
+
+MYSQL_COMMAND="mysql -h$DB_HOST -P$DB_PORT -u$USERNAME $DATABASE"
+## Updates
+echo "> Processing database updates ..."
+COUNT=0
+for UPDATE in "${ADDITIONAL_PATH}Updates"[0-9]*.sql
+do
+  if [ -e "$UPDATE" ]
+  then
+    echo "    Appending $UPDATE"
+    #$MYSQL_COMMAND < "$UPDATE"
+    #if [[ $? != 0 ]]
+    #then
+    #  echo "ERROR: cannot apply $UPDATE"
+    #  exit 1
+    #fi
+    ((COUNT++))
+  fi
+done
+if [ "$COUNT" != 0 ]
+then
+  echo "  $COUNT DB updates applied successfully"
+else
+  echo "  Did not found any new DB update to apply"
+fi
